ac39221dccd98b9567e3e8a0287cdf7a4889130d
desbma <desbma@users.noreply.github.com>
2016-02-07 23:49:12 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(name="pyfastcopy",
                    "Operating System :: Unix",
                    "Programming Language :: Python",
                    "Programming Language :: Python :: 2",
-                   "Programming Language :: Python :: 2.7"
+                   "Programming Language :: Python :: 2.7",
                    "Programming Language :: Python :: 3",
                    "Programming Language :: Python :: 3.0",
                    "Programming Language :: Python :: 3.1",
